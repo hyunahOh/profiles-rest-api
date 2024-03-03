ec786cc67a8d5412cb788a44fc67204c8879e815
hyunahOh <dowkkr94@gmail.com>
2024-03-03 13:47:53 +0900

    ADD apis

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from profiles_api import views
 
 
-url_patterns = [
+urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view())
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializer
 
 
 class HelloApiView(APIView):
+    serializer_class = serializer.HelloSerializer
 
     def get(self, request, format=None):
         an_apiview = [
@@ -12,3 +16,13 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            return Response({
+                'message': f'Hello {serializer.validated_data.get('name')}'
+            })
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
